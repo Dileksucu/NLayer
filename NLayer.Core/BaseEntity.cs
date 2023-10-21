5e39e98878264a216211683e13096e69aeb4534a
Dilek sucu <77547779+Dileksucu@users.noreply.github.com>
2023-10-22 01:54:33 +0300

    Seed Data Changes

--- NLayer.Core/BaseEntity.cs
+++ NLayer.Core/BaseEntity.cs
@@ -14,7 +14,7 @@ namespace NLayer.Core
         //otomatik olarak PK olarak tanımlanabilmesini sağlamak.
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
 
         //Abstract yapmamızın sebebi ; newlemeden kullanmak için abstract kullandık.
         //Bu nesne tipi soyut yapılardır ; ınterfaceler gibi. 
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            //bu şekilde vt da tablo oluştuğunda bu verileri direkt ekler.Default olarak)
+            builder.HasData
+            (
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Name = "Tükenmez Kalem",
+                    Price = 500,
+                    Stock = 10,
+                    CreatedDate = DateTime.Now,
+                },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 1,
+                    Name = "Kurşun Kalem",
+                    Price = 150,
+                    Stock = 1000,
+                    CreatedDate = DateTime.Now,
+                },
+                new Product
+                {
+                     Id = 3,
+                     CategoryId = 2,
+                     Name = "Telli Defter",
+                     Price = 150,
+                     Stock = 100,
+                     CreatedDate = DateTime.Now,
+                }
+            );
+        }
+    }
+}
